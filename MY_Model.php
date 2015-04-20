a47adebfa5799c751b3cf031ed551f0a26db3127
Adrian Voicu <avenir.ro@gmail.com>
2015-04-20 10:52:14 +0300

    added "where_in" on pivot table
    issue #11

--- MY_Model.php
+++ MY_Model.php
@@ -581,6 +581,7 @@ class MY_Model extends CI_Model
             {
                 $this->_database->join($pivot_table, $foreign_table.'.'.$foreign_key.' = '.$pivot_table.'.'.singular($foreign_table).'_'.$foreign_key, 'right');
                 $this->_database->join($this->table, $pivot_table.'.'.singular($this->table).'_'.$local_key.' = '.$this->table.'.'.$local_key,'right');
+                $this->_database->where_in($this->table.'.'.$local_key,$local_key_values);
                 $sub_results = $this->_database->get($foreign_table)->result_array();
             }
 
