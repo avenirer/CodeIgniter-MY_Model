37af7517036bde197591a965cf9ba579789207a7
Adrian Voicu <avenir.ro@gmail.com>
2015-06-05 12:18:35 +0300

    Explanation of the from_form() method
    ...

--- MY_Model.php
+++ MY_Model.php
@@ -259,6 +259,17 @@ class MY_Model extends CI_Model
         return $data;
     }
 
+    /**
+     * public function from_form($rules = NULL,$additional_values = array(), $row_fields_to_update = array())
+     * Gets data from form, after validating it and waits for an insert() or update() method in the query chain 
+     * @param null $rules Gets the validation rules. If nothing is passed (NULL), will look for the validation rules
+     * inside the model $rules public property
+     * @param array $additional_values Accepts additional fields to be filled, fields that are not to be found inside
+     * the form. The values are inserted as an array with "field_name" => "field_value"
+     * @param array $row_fields_to_update You can mention the fields from the form that can be used to identify
+     * the row when doing an update
+     * @return $this
+     */
     public function from_form($rules = NULL,$additional_values = array(), $row_fields_to_update = array())
     {
         $this->_get_table_fields();
@@ -289,7 +300,7 @@ class MY_Model extends CI_Model
                     }
                 }
             }
-            
+
             if(!empty($row_fields_to_update))
             {
                 foreach ($row_fields_to_update as $field) {
