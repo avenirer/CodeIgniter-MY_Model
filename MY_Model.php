0a410d4a8065f0550a63160f052c2e0b41702c4e
Adrian Voicu <avenir.ro@gmail.com>
2015-06-10 12:32:36 +0300

    testing
    testing

--- MY_Model.php
+++ MY_Model.php
@@ -869,7 +869,7 @@ class MY_Model extends CI_Model
      * Joins the subquery results to the main $data
      * @param $data
      * @return mixed
-     */
+     *
     protected function join_temporary_results($data)
     {
         foreach($this->_requested as $requested_key => $request)
@@ -937,6 +937,103 @@ class MY_Model extends CI_Model
                 $subs = array();
                 foreach ($sub_results as $result) {
                     $subs[$result[$foreign_key]][] = $result;
+                }
+                $sub_results = $subs;
+                foreach($local_key_values as $key => $value)
+                {
+                    if(array_key_exists($value,$sub_results))
+                    {
+                        if ($type == 'has_one')
+                        {
+                            $data[$key][$relation_key] = $sub_results[$value][0];
+                        }
+                        else
+                        {
+                            $data[$key][$relation_key] = $sub_results[$value];
+                        }
+                    }
+                }
+            }
+            unset($this->_requested[$requested_key]);
+        }
+        return $data;
+        //if(sizeof($data)==1) $data = $data[0];
+        //return ($this->return_as == 'object') ? json_decode(json_encode($data), FALSE) : $data;
+    }
+    */
+    
+    protected function join_temporary_results($data)
+    {
+        foreach($this->_requested as $requested_key => $request)
+        {
+            $pivot_table = NULL;
+            $relation = $this->_relationships[$request['request']];
+            $this->load->model($relation['foreign_model']);
+            $foreign_key = $relation['foreign_key'];
+            $local_key = $relation['local_key'];
+            (isset($relation['pivot_table'])) ? $pivot_table = $relation['pivot_table'] : FALSE;
+            $foreign_table = $relation['foreign_table'];
+            $type = $relation['relation'];
+            $relation_key = $relation['relation_key'];
+            $local_key_values = array();
+            foreach($data as $key => $element)
+            {
+                if(isset($element[$local_key]))
+                {
+                    $id = $element[$local_key];
+                    $local_key_values[$key] = $id;
+                }
+            }
+            if(!isset($pivot_table))
+            {
+                $sub_results = $this->{$relation['foreign_model']}->as_array();
+                if(!empty($request['parameters']))
+                {
+                    $sub_results = (array_key_exists('fields',$request['parameters'])) ? $sub_results->fields($request['parameters']['fields'].','.$foreign_table.'.'.$foreign_key) : $sub_results;
+                    $sub_results = (array_key_exists('where',$request['parameters'])) ? $sub_results->where($request['parameters']['where'],NULL,NULL,FALSE,FALSE,TRUE) : $sub_results;
+                }
+                $sub_results = $sub_results->where($foreign_key, $local_key_values)->get_all();
+            }
+            else
+            {
+                $this->_database->join($pivot_table, $foreign_table.'.'.$foreign_key.' = '.$pivot_table.'.'.singular($foreign_table).'_'.$foreign_key, 'inner');
+                $this->_database->join($this->table, $pivot_table.'.'.singular($this->table).'_'.$local_key.' = '.$this->table.'.'.$local_key,'inner');
+                // testing
+                if(!empty($request['parameters']))
+                {
+                    if(array_key_exists('fields',$request['parameters']))
+                    {
+                        $fields = explode(',',$request['parameters']['fields']);
+                        $select = array();
+                        foreach($fields as $field)
+                        {
+                            $select[] = '`'.$foreign_table.'`.`'.trim($field).'`';
+                        }
+                        $the_select = implode(',',$select);
+                        $this->_database->select($the_select);
+                        $this->_database->select($foreign_table.'.'.$foreign_key);
+                    }
+
+                    if(array_key_exists('where',$request['parameters']))
+                    {
+                        $this->_database->where($request['parameters']['where'],NULL,NULL,FALSE,FALSE,TRUE);
+                    }
+                }
+                // end testing
+                $this->_database->where_in($this->table.'.'.$local_key,$local_key_values);
+                $sub_results = $this->_database->get($foreign_table)->result_array();
+                $this->_database->reset_query();
+            }
+
+            if(isset($sub_results) && !empty($sub_results)) {
+                $subs = array();
+                foreach ($sub_results as $result) {
+                    $the_foreign_key = $result[$foreign_key];
+                    if(strstr($request['parameters']['fields'], $foreign_table.'.'.$foreign_key))
+                    {
+                        unset($result->{$foreign_key});
+                    }
+                    $subs[$the_foreign_key][] = $result;
                 }
                 $sub_results = $subs;
                 foreach($local_key_values as $key => $value)
