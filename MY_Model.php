09357c87db85c29300d7ae95023b0035e568983a
Adrian Voicu <avenir.ro@gmail.com>
2015-07-07 13:47:49 +0300

    LICENCE

--- MY_Model.php
+++ MY_Model.php
@@ -1,5 +1,26 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-
+/*
+* Copyright (C) 2014 @avenirer [avenir.ro@gmail.com]
+* Everyone is permitted to copy and distribute verbatim or modified copies of this license document, 
+* and changing it is allowed as long as the name is changed.
+* DON'T BE A DICK PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+*
+***** Do whatever you like with the original work, just don't be a dick.
+***** Being a dick includes - but is not limited to - the following instances:
+********* 1a. Outright copyright infringement - Don't just copy this and change the name.
+********* 1b. Selling the unmodified original with no work done what-so-ever, that's REALLY being a dick.
+********* 1c. Modifying the original work to contain hidden harmful content. That would make you a PROPER dick.
+***** If you become rich through modifications, related works/services, or supporting the original work, share the love. Only a dick would make loads off this work and not buy the original works creator(s) a pint.
+***** Code is provided with no warranty. 
+*********** Using somebody else's code and bitching when it goes wrong makes you a DONKEY dick. 
+*********** Fix the problem yourself. A non-dick would submit the fix back.
+ * 
+ * 
+ * filename: Matches.php
+ * This project started from a great idea posted by @veedeoo [veedeoo@gmail.com] on http://www.daniweb.com/web-development/php/code/477847/codeigniter-cli-trainer-script-creates-simple-application
+ * License info: http://www.dbad-license.org/
+ */
+ 
 /** how to extend MY_Model:
  *	class User_model extends MY_Model
  *	{
