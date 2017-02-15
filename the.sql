35631e956c651152f61717d1c1cecbb1327758ea
Adrian Voicu <avenir.ro@gmail.com>
2017-02-15 10:11:46 +0200

    some additional fields for testing

--- the.sql
+++ the.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.6.5.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 04, 2017 at 04:31 PM
--- Server version: 5.6.33
--- PHP Version: 7.0.12
+-- Generation Time: Feb 15, 2017 at 09:10 AM
+-- Server version: 5.6.34
+-- PHP Version: 7.1.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `mymodel`
@@ -22,19 +28,36 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `articles` (
   `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
+  `title` varchar(255) NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `articles`
 --
 
-INSERT INTO `articles` (`id`, `title`) VALUES
-(1, 'First article title'),
-(2, 'Another article title'),
-(3, 'One more article title'),
-(4, 'This article has a title too'),
-(5, 'How about this article title');
+INSERT INTO `articles` (`id`, `title`, `deleted_at`, `deleted_by`, `updated_at`, `updated_by`, `created_at`, `created_by`) VALUES
+(1, 'First article title', NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'Another article title', NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 'One more article title', NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'This article has a title too', NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 'How about this article title', NULL, NULL, NULL, NULL, NULL, NULL),
+(6, 'Test to be deleted', NULL, NULL, NULL, NULL, '2017-02-15 08:18:14', NULL),
+(7, 'Test to be deleted', NULL, NULL, NULL, NULL, '2017-02-15 08:19:19', NULL),
+(8, 'Test to be deleted', '2017-02-15 08:20:39', NULL, NULL, NULL, '2017-02-15 08:20:39', NULL),
+(9, 'Test to be deleted', '2017-02-15 08:26:44', NULL, NULL, NULL, '2017-02-15 08:26:44', NULL),
+(10, 'Test to be deleted', '2017-02-15 08:28:33', NULL, NULL, NULL, '2017-02-15 08:28:33', NULL),
+(11, 'Test to be deleted', '2017-02-15 08:28:37', NULL, NULL, NULL, '2017-02-15 08:28:37', NULL),
+(12, 'Test to be deleted', '2017-02-15 08:30:10', NULL, NULL, NULL, '2017-02-15 08:30:10', NULL),
+(13, 'Test to be deleted', '2017-02-15 08:31:22', NULL, NULL, NULL, '2017-02-15 08:31:22', NULL),
+(14, 'Test to be deleted', '2017-02-15 09:01:01', NULL, NULL, NULL, '2017-02-15 09:01:01', NULL),
+(15, 'Test to be deleted', '2017-02-15 09:01:22', NULL, NULL, NULL, '2017-02-15 09:01:22', NULL),
+(16, 'Test to be deleted', '2017-02-15 09:02:42', NULL, NULL, NULL, '2017-02-15 09:02:42', NULL);
 
 -- --------------------------------------------------------
 
@@ -199,7 +222,7 @@ ALTER TABLE `user_details`
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `posts`
 --
@@ -210,3 +233,6 @@ ALTER TABLE `posts`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
