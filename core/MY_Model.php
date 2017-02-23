27c0d6612b297d85b902c9e7fa42f4c9214a22fc
lloricode <emorickfighter@gmail.com>
2017-02-23 21:52:58 +0800

    add function to remove empty value in field, also error messages

--- core/MY_Model.php
+++ core/MY_Model.php
@@ -352,11 +352,13 @@ class MY_Model extends CI_Model
         {
             $this->fillable_fields();
             $this->validated = array();
-            foreach($rules as $rule)
+            foreach($rules as $table_column_name => $rule)
             {
-                if(in_array($rule['field'],$this->_can_be_filled))
+                if(in_array($table_column_name,$this->_can_be_filled))
                 {
-                    $this->validated[$rule['field']] = $this->input->post($rule['field']);
+                    $this->validated[$table_column_name] = $this->input->post($rule['field']);
+                }else{                        
+                     show_error('MY_Model: Unknown column ('.$table_column_name.') in table: ('.$this->table.') in $rules.');
                 }
             }
             if(isset($additional_values) && is_array($additional_values) && !empty($additional_values))
@@ -366,7 +368,9 @@ class MY_Model extends CI_Model
                     if(in_array($field, $this->_can_be_filled))
                     {
                         $this->validated[$field] = $value;
-                    }
+                    }else{                        
+                     show_error('MY_Model: Unknown column ('.$field.') in table: ('.$this->table.') in $additional_values.');
+                }
                 }
             }
 
@@ -379,9 +383,9 @@ class MY_Model extends CI_Model
                     else if (in_array($key, $this->table_fields)){
                         $this->row_fields_to_update[$key] = $field;
                     }
-                    else {
-                        continue;
-                    }
+                    else {  
+                        show_error('MY_Model: Unknown column ('.$key.'=>'.$field.') in table: ('.$this->table.') in $row_fields_to_update.');
+                     }
                 }
             }
             return $this;
@@ -393,7 +397,32 @@ class MY_Model extends CI_Model
         }
 
     }
-
+    private function _unset_none_value_fields($data)
+    {
+        $multi = $this->is_multidimensional($data);
+         if($multi===FALSE)
+        {
+            foreach ($data as $field => $value)
+            {
+                if(trim($value)==''){
+                        unset($data[$field]);
+                }
+            }
+        }
+        else
+        {
+            foreach($data_as_array as $key => $row)
+            {
+                foreach ($row as $field => $value)
+                {
+                    if(trim($value)==''){
+                        unset($data[$field]);
+                    }
+                }
+            }
+        }
+        return $data;
+    }
     /**
      * public function insert($data)
      * Inserts data into table. Can receive an array or a multidimensional array depending on what kind of insert we're talking about.
@@ -412,7 +441,8 @@ class MY_Model extends CI_Model
             return FALSE;
         }
         $data = $this->_prep_before_write($data);
-
+        $data = $this->_unset_none_value_fields($data);
+    
         //now let's see if the array is a multidimensional one (multiple rows insert)
         $multi = $this->is_multidimensional($data);
 
@@ -1220,7 +1250,7 @@ class MY_Model extends CI_Model
                     if(array_key_exists('with',$request['parameters']))
                     {
                         // Do we have many nested relation
-                        if(is_array($request['parameters']['with']) && isset($request['parameters']['with'][0])&& is_array($request['parameters']['with'][0]))
+                        if(is_array($request['parameters']['with']) && isset($request['parameters']['with'][0]))
                         {
                             foreach ($request['parameters']['with'] as $with)
                             {
